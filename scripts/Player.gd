d5a10fc675a4ff7126e04408a3bb3ed83a6f2203
dpere122 <dpere381@fiu.edu>
2023-06-23 18:09:59 -0400

    Added wait time between player death and restart screen

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -8,6 +8,14 @@ extends CharacterBody2D
 @export var damage : int = 50
 @export var weapon_filter : Color = Color(0,0,0,0)
 
+var ship_sprite_path : String = ""
+
+func _ready()->void:
+	#This can ensure certain features are available if componenets are added
+	#Though inheritance could probably be used instead to create a hiearchical
+	#structure about the basic function of these objects
+	ship_sprite_path = $Space_Ship.get_path()
+
 func _input(event)->void:
 	if(event.is_action_pressed("ui_console")):
 		toggle_pause()
@@ -47,15 +55,26 @@ func shootMainGun()-> Node2D:
 	return bullet_node
 	
 func flash()-> void:
-	$Space_Ship.material.set_shader_parameter("flash_mod",1)
-	$eff_timer.start()
+	if(get_node_or_null(ship_sprite_path) != null):
+		$Space_Ship.material.set_shader_parameter("flash_mod",1)
+		$eff_timer.start()
 	
 func destroyed() -> void:
 	#this function will be called by the bullet that causes hp to go below 0
 	var explosion: PackedScene = load("res://static_entity/explosion.tscn")
 	var explosion_node: Node2D = explosion.instantiate()
 	explosion_node.position = get_global_transform().origin
+	$Space_Ship.queue_free()	
 	get_node("/root").add_child(explosion_node)
+	
+	var t = Timer.new()
+	t.set_wait_time(5)
+	t.set_one_shot(true)
+	self.add_child(t)
+	t.start()
+	await t.timeout
+	t.queue_free()
+	
 	get_tree().change_scene_to_file("res://Levels/end_scene.tscn")
 	queue_free()
 
@@ -66,7 +85,8 @@ func take_damage(amount: int) -> void:
 		destroyed()
 
 func _on_eff_timer_timeout()-> void:
-	$Space_Ship.material.set_shader_parameter("flash_mod",0)
+	if(get_node_or_null(ship_sprite_path) != null):
+		$Space_Ship.material.set_shader_parameter("flash_mod",0)
 
 func toggle_pause() -> void:
 	if(!get_tree().paused):
